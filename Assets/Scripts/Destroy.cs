8cc14771877eb7d512a810dcb032071be6ab3776
jgl58 <jonay2015>
2020-04-10 19:15:48 +0200

    Creado mapa precario y movimiento con tecla

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+        Destroy(gameObject);
+        
+    }
+    
+}
---
+++ Assets/Scripts/GeneraSuelo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneraSuelo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject bloque;
+    public GameObject nieve;
+
+    public GameObject suelo;
+
+
+    void Start()
+    {
+        for(int i = -8; i <= 60; i += 2)
+        {
+            
+            int pongoPremio = Random.Range(0, 3);
+            if (pongoPremio == 1)
+            {
+                Instantiate(bloque, new Vector3(0, 0, i), new Quaternion()).transform.parent = suelo.transform;
+            }
+            else
+            {
+
+                Instantiate(nieve, new Vector3(0, 0, i), new Quaternion()).transform.parent = suelo.transform;
+            }
+
+        }
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
