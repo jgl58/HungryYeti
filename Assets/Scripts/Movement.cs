328c923ee1f986da576abd611f361b1b01ca0b82
Sergio <smg158@alu.ua.es>
2020-04-15 18:38:58 +0200

    añadido el tap

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,10 @@ public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     private int refreshCounter = 5;
+
+    private Touch touch;
+    private Vector2 beginTouchPosition, endTouchPosition;
+
     void Start()
     {
         
@@ -30,6 +34,33 @@ public class Movement : MonoBehaviour
             }
 
             
+        }
+
+        if(Input.touchCount > 0){
+            touch = Input.GetTouch(0);
+            
+            switch(touch.phase){
+                case TouchPhase.Began:
+                    beginTouchPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    endTouchPosition = touch.position;
+                    if(beginTouchPosition == endTouchPosition){
+                        Vector3 position = this.transform.position;
+                        position.z--;
+                        this.transform.position = position;
+                        refreshCounter--;
+                        if (refreshCounter == 0)
+                        {
+                            refreshCounter = 5;
+                            print("Cargamos nuevos bloques");
+
+
+                            BloquesFactory.generateSuelo(5);
+                        }
+                    }
+                break;    
+            }
         }
     }
 }
