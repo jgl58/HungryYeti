84355435694fa8608e7018f3e8dbf8a43ae7dd19
Sergio <smg158@alu.ua.es>
2020-05-05 21:32:03 +0200

    creado el menu de inicio

---
+++ Assets/Scripts/StartButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButtonHandler : MonoBehaviour
+{
+    public GameObject tituloLabel;
+    public Button yourButton;
+
+    private GameObject hud;
+    private GameObject player;
+
+	void Start () {
+        hud = (GameObject)Resources.Load("Prefabs/GameCanvas");
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+        tituloLabel.SetActive(false);
+        yourButton.gameObject.SetActive(false);
+        Instantiate(hud, new Vector3(0, 0, 0), Quaternion.identity);
+        Globals.estado = Globals.gameState.jugando;
+	}
+
+}
