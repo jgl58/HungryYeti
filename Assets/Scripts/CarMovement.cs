109a570a2d21901dd12de4ece10c4517c2e44f2e
Flaviu Lucian Georgiu <flaviu.georgiu97@gmail.com>
2020-05-07 17:20:27 +0200

    carreteras y coches basicos

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 1.0f;
+    private Vector3 startPos;
+    bool goRight = true;
+    void Start()
+    {
+        startPos = transform.position;
+        if (startPos.x > 0)
+        {
+            goRight = false;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 v = transform.position;
+        if (!goRight)
+        {
+            if (transform.position.x <= -7.0f)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                v.x -= (Time.deltaTime * speed);
+            }
+            v.z = transform.parent.position.z;
+            transform.position = v;
+        }
+        else
+        {
+            if (transform.position.x >= 7.0f)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                v.x += (Time.deltaTime * speed);
+            }
+            v.z = transform.parent.position.z;
+            transform.position = v;
+        }
+        
+        
+    }
+}
